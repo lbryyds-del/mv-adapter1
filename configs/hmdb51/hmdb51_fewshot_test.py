ea4f9846e8eaea4e4e69eb2ad755358add5fbd62
lbryyds-del <l826604001@gmail.com>
2025-11-24 20:07:08 +0800

    Update hmdb51_fewshot_test.py

--- configs/hmdb51/hmdb51_fewshot_test.py
+++ configs/hmdb51/hmdb51_fewshot_test.py
@@ -57,7 +57,7 @@ model = dict(
                 cls_mid=64,
                 n_head=2,
                 seq_len=max_frames,
-                temporal=['c', 'p'],
+                temporal=['c'],
                 pca=False,
             )
         ),
